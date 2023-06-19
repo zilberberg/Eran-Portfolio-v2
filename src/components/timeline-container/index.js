7f5239de9c0510b5b6ab3e788fa432a598a07269
zilberberg <zil.eran@gmail.com>
2023-06-19 09:44:11 +0300

    added timeline pcg, added experience content

--- src/components/timeline-container/index.js
+++ src/components/timeline-container/index.js
@@ -1,9 +1,59 @@
 import React from "react"
+import Timeline from '@mui/lab/Timeline'
+import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem'
+import TimelineSeparator from '@mui/lab/TimelineSeparator'
+import TimelineConnector from '@mui/lab/TimelineConnector'
+import TimelineContent from '@mui/lab/TimelineContent'
+import TimelineDot from '@mui/lab/TimelineDot'
+import { Typography } from "@mui/material"
+import { CompanyStyle, DatesStyle, DescriptionStyle, PositionStyle } from "./styles"
 
 const TimelineContainer = (props) => (
-    <div>
+    <Timeline
+        sx={{
+            [`& .${timelineItemClasses.root}:before`]: {
+            flex: 0,
+            padding: 0,
+            },
+        }}
+    >
+        {
+            props.data.map((work, index) => {
+                return (
+                    <TimelineItem key={index}>
+                        <TimelineSeparator>
+                            <TimelineDot />
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: '8px', maxWidth: "300px" }}>
+                            <Typography component="span" sx={DatesStyle}>
+                                {work.dates}
+                            </Typography>
+                        </TimelineContent>
 
-    </div>
+                        <TimelineContent sx={{ py: '8px', px: 2, marginBottom: '40px' }}>
+                            <Typography component="div" sx={{ marginBottom: '10px' }}>
+                                <Typography component="span" sx={PositionStyle}>
+                                    {work.position}
+                                </Typography>
+                            </Typography>
+                            <Typography component="div" sx={{ marginBottom: '40px' }}>
+                                <Typography component="span" sx={CompanyStyle}>
+                                    {work.company}&nbsp;
+                                </Typography>
+                                <Typography component="span" sx={CompanyStyle}>
+                                    {work.location}
+                                </Typography>
+                            </Typography>
+                            <Typography component="span" sx={DescriptionStyle}>
+                                {work.description}
+                            </Typography>
+                        </TimelineContent>
+                    </TimelineItem>
+                )
+            })
+        }
+    </Timeline>
 )
 
 export default TimelineContainer;
---
+++ src/components/timeline-container/styles.js
@@ -0,0 +1,31 @@
+export const DatesStyle = {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: '20px',
+    lineHeight: '23px',
+}
+
+export const PositionStyle = {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: '24px',
+    lineHeight: '28px',
+}
+
+export const CompanyStyle = {
+    fontFamily: 'Montserrat',
+    fontStyle: 'italic',
+    fontWeight: '400',
+    fontSize: '20px',
+    lineHeight: '24px',
+}
+
+export const DescriptionStyle = {
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: '20px',
+    lineHeight: '24px',
+}
